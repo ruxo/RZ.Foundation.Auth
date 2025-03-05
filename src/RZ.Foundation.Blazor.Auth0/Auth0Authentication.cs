73dd79ac5391900c8428ca1dc9ab343a21a4d0bf
Ruxo Zheng <rz@just.sent.as>
2025-03-05 08:28:52 +0700

    Handle after signin

--- src/RZ.Foundation.Blazor.Auth0/Auth0Authentication.cs
+++ src/RZ.Foundation.Blazor.Auth0/Auth0Authentication.cs
@@ -1,6 +1,7 @@
 ﻿using Auth0.AspNetCore.Authentication;
 using JetBrains.Annotations;
 using LanguageExt;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,7 @@ using RZ.AspNet;
 using RZ.Foundation.Blazor.Auth;
 using RZ.Foundation.Helpers;
 using RZ.Foundation.Types;
+using TiraxTech;
 
 namespace RZ.Foundation.Blazor.Auth0;
 
@@ -23,6 +25,36 @@ public static class AUth0Authentication
                     var dict = KeyValueString.Parse(builder.Configuration.GetConnectionString(configName) ?? throw new ErrorInfoException(StandardErrorCodes.MissingConfiguration));
                     opts.Domain = dict["Domain"] ?? throw new ErrorInfoException(StandardErrorCodes.MissingConfiguration);
                     opts.ClientId = dict["ClientId"] ?? throw new ErrorInfoException(StandardErrorCodes.MissingConfiguration);
+
+                    opts.OpenIdConnectEvents = new OpenIdConnectEvents {
+                        OnTicketReceived = async context => {
+                            Console.WriteLine("OnTicketReceived");
+                            if (context.HttpContext.RequestServices.GetService<IAfterSignInHandler>() is not {} afterSignInHandler)
+                                return;
+
+                            var user = context.Principal!;
+                            var result = await afterSignInHandler.ProceedAfterSignInFlow(user);
+                            switch (result){
+                                case AfterSignInCheck.Failed failed:
+                                    context.Fail(failed.Message);
+                                    break;
+
+                                case AfterSignInCheck.CustomLoginFlow custom:
+                                    var path = context.Properties?.RedirectUri is {} url? custom.CustomFlowPath.UpdateQuery("returnUrl", url) : custom.CustomFlowPath;
+                                    context.Response.Redirect(path.ToString());
+                                    context.HandleResponse();
+                                    break;
+
+                                case AfterSignInCheck.LoginSuccess login:
+                                    context.Principal = login.User;
+                                    break;
+
+                                default:
+                                    context.Fail($"Unknown {nameof(AfterSignInCheck)} type: {result}");
+                                    break;
+                            }
+                        }
+                    };
                 });
         return builder;
     }
